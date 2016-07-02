58505c6f7669a1a29d00f16e399c5c191fe848a4
jiaw0 <wangjiawei0722@qq.com>
2016-07-02 11:06:40 +0800

    baidulasdlx

---
+++ wlstore/js/index.js
@@ -0,0 +1,9 @@
+$(function(){
+	var mySwiper=new Swiper('swiper-container',{
+		direction:"horizontal",
+		prevButton:"swiper-button-prev",
+		nextButton:"swiper-button-next",
+		speed:300,
+		autoplay:1000
+	});
+})
